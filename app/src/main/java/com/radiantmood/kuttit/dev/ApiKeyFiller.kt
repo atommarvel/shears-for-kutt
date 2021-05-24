c8da06189a2b83a3773fdca360d791bef7c930c8
atommarvel <atom.raiff@gmail.com>
2021-05-24 02:59:17 -0400

    dev method

---
+++ app/src/main/java/com/radiantmood/kuttit/dev/ApiKeyFiller.kt
@@ -0,0 +1,8 @@
+package com.radiantmood.kuttit.dev
+
+/**
+ * For faster development. Fill out string with your API key. Don't commit your key though!
+ * - ignore with git: git update-index --assume-unchanged <file>
+ * - revert ignorance: git update-index --no-assume-unchanged <file>
+ */
+fun getApiKeyOrEmpty() = ""
